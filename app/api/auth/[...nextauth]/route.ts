8fa9a0d0cef18ce893ffad1e922a4d25463502ec
slimene <slimene.elkhames@gmail.com>
2025-04-15 22:42:10 +0100

    mise a jour final

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,14 @@
-import NextAuth from 'next-auth';
+/* import NextAuth from 'next-auth';
 import { authConfig } from '@/auth.config';
 
 const handler = NextAuth(authConfig);
 
 export const GET = handler;
-export const POST = handler;
+export const POST = handler; */
+import NextAuth from 'next-auth';
+import { authConfig } from '@/auth.config';
+
+const handler = NextAuth(authConfig);
+
+// Exporter chaque méthode comme fonction
+export { handler as GET, handler as POST };
